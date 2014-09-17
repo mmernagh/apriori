1cf3405242f206a61185c72ee9fc44ff00cb86e0
kfritschie <fritschie.1@osu.edu>
2014-09-17 15:12:51 -0400

    new class for addcontenthandler

---
+++ data_mining/src/AddContentHandler.java
@@ -0,0 +1,39 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.Set;
+
+/**This content handler consumes without performing any processing, similar to IgnoreContentHandler
+ * Created by kfritschie on 9/17/2014.
+ */
+public class AddContentHandler extends DefaultHandler{
+
+    private int depth = 1;
+    private XMLReader xmlReader;
+    private ContentHandler contentHandler;
+    private Set<String> toPlStrSet;
+
+    public AddContentHandler (XMLReader xmlReader, ContentHandler contentHandler, Set<String> topPlStrSet){
+        this.contentHandler = contentHandler;
+        this.xmlReader = xmlReader;
+        this.toPlStrSet = topPlStrSet;
+    }
+    @Override public void characters(char ch[], int start, int length){
+
+    toPlStrSet.add(new String(ch, start, length));
+    }
+
+    @Override public void startElement ( String uri, String localName, String qName, Attributes atts){
+        depth++;
+    }
+    public void endElement (String uri, String localName, String qName)
+      throws SAXException{
+        depth --;
+        if(0 == depth){
+            xmlReader.setContentHandler(contentHandler);
+        }
+    }
+}
