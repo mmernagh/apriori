ba44a321387fc85710abf6999963fa6577847d46
mernagh <mernagh@bitbucket.org>
2014-11-12 19:27:07 -0500

    added initial files

---
+++ minhash/minhash/Main.java
@@ -0,0 +1,14 @@
+package minhash;
+
+/**
+ * Generates the Jaccard similarity of all feature vectors pairwise, and compares this to
+ * a min-hash comparison of all feature vectors.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ minhash/minhash/Minhasher.java
@@ -0,0 +1,5 @@
+package minhash;
+
+public class Minhasher {
+
+}
